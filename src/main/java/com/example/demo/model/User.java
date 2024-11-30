c20d3a2409fd0b163f6fa885a9713fad1a63d97d
timur_gribov <timur.aymal@mail.ru>
2024-11-30 16:09:46 +0300

    first commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+}
